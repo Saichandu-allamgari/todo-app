4728a228de420549059ff63c140283aa69bb5b8d
unknown <saichanduallamgari@gmail.com>
2025-03-27 18:34:25 +0530

    12 commit

--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -5,41 +5,41 @@ import AuthForm from "../components/AuthForm";
 const Login = () => {
   const navigate = useNavigate();
 
-  // const handleLogin = async (formData) => {
-  //   try {
-  //     await login(formData);
-  //     navigate("/dashboard"); // Redirect to Dashboard if login is successful
-  //   } catch (error) {
-  //     if (error.response && error.response.status === 401) {
-  //       // If user doesn't exist, redirect to Register page
-  //       alert("User not found. Redirecting to registration...");
-  //       navigate("/register");
-  //     } else {
-  //       alert("Login failed! Please check your credentials.");
-  //     }
-  //   }
-  // };
-
   const handleLogin = async (formData) => {
     try {
-      const response = await login(formData);
-      
-      const sessionCheck = await getUser(); // ✅ Check session immediately after login
-      if (sessionCheck.data) {
-
-        
-        // navigate("/dashboard");
-      } else {
-        alert("Session not set. Please try logging in again.");
-      }
+      await login(formData);
+      // navigate("/dashboard"); // Redirect to Dashboard if login is successful
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        alert("Invalid credentials");
+        // If user doesn't exist, redirect to Register page
+        alert("User not found. Redirecting to registration...");
+        navigate("/register");
       } else {
-        alert("Login failed! Please try again later.");
+        alert("Login failed! Please check your credentials.");
       }
     }
   };
+
+  // const handleLogin = async (formData) => {
+  //   try {
+  //     const response = await login(formData);
+      
+  //     const sessionCheck = await getUser(); // ✅ Check session immediately after login
+  //     if (sessionCheck.data) {
+
+        
+  //       // navigate("/dashboard");
+  //     } else {
+  //       alert("Session not set. Please try logging in again.");
+  //     }
+  //   } catch (error) {
+  //     if (error.response && error.response.status === 401) {
+  //       alert("Invalid credentials");
+  //     } else {
+  //       alert("Login failed! Please try again later.");
+  //     }
+  //   }
+  // };
   
 
   return (<><div className="flex justify-end m-6">
